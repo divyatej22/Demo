9391bb009871ef34ae08029a7f2d160a3721bcb0
Dake Divyatej <dakedivyatej2004@gamil.com>
2024-12-20 15:18:29 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+export default function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    <Route>
+<Route path="/" element={<MyLogin/>}
+
+
+
+    </Route>
+    </div>
+  )
+}
